00dfd44fc99c3b46415f5b7b09fd8ec5f354c7bc
z1zonemoi <zizonemoi@gmail.com>
2022-09-12 01:43:39 +0900

    scss 모듈 include-media, open-color

---
+++ scss.js
@@ -0,0 +1,7 @@
+//반응형 디자인을 쉽게 만들어 주는 include-media
+//색상 팔레트 open-color
+//npm i open-color include-media
+//utils.scss파일에
+//@import '~include-media/dist/include-media';
+//@import '~open-color/open-color';
+//작성하기
